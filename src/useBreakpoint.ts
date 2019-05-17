1241bb8f22e0d759c09185bd0092b42d4c292763
Thomas Thiebaud <thiebaud.tom@gmail.com>
2019-05-17 19:42:39 +0200

    feat: add useBreakpoint and useBreakpoints hooks

---
+++ src/useBreakpoint.ts
@@ -0,0 +1,12 @@
+import { useWindowSize } from './useWindowSize';
+
+export function useBreakpoint(breakpoint: number) {
+  const { width } = useWindowSize();
+  return width < breakpoint;
+}
+
+export function useBreakpoints(breakpoints: number[]) {
+  const { width } = useWindowSize();
+
+  return breakpoints.map(breakpoint => breakpoint < width);
+}
