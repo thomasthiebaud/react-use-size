0c5c682d4ebc4a57c5a21b539ac0e4159e6f2976
Thomas Thiebaud <thiebaud.tom@gmail.com>
2019-05-25 20:46:46 +0200

    chore: apply linter in all typescript files

--- tests/useBreakpoint.spec.tsx
+++ tests/useBreakpoint.spec.tsx
@@ -5,102 +5,118 @@ import { act } from 'react-dom/test-utils';
 import { useBreakpoint, useBreakpoints } from '../src';
 
 describe('useBreakpoint', () => {
-    let container: Element;
+  let container: Element;
 
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+  });
+
+  it('should switch component based on a single breakpoint', () => {
+    const Component = () => {
+      const isSmall = useBreakpoint(500);
+
+      if (isSmall) {
+        return <p id="small">Small</p>;
+      }
+
+      return <p id="big">Big</p>;
+    };
+
+    act(() => {
+      ReactDOM.render(<Component />, container);
     });
 
-    afterEach(() => {
-        document.body.removeChild(container);
+    let big = container.querySelector('#big');
+    let small = container.querySelector('#small');
+    expect(big && big.textContent).toEqual('Big');
+    expect(small).toBeNull();
+
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 400,
     });
 
-    it('should switch component based on a single breakpoint', () => {
-        const Component = () => {
-            const isSmall = useBreakpoint(500);
+    act(() => {
+      window.dispatchEvent(new Event('resize'));
+    });
 
-            if (isSmall) {
-                return <p id="small">Small</p>
-            }
+    big = container.querySelector('#big');
+    small = container.querySelector('#small');
+    expect(small && small.textContent).toEqual('Small');
+    expect(big).toBeNull();
+  });
 
-            return <p id="big">Big</p>
-        }
-        
-        act(() => {
-            ReactDOM.render(<Component />, container);
-        });
+  it('should switch component based on multiple breakpoints', () => {
+    const Component = () => {
+      const [isSmall, isMedium] = useBreakpoints([500, 700]);
 
-        let big = container.querySelector('#big');
-        let small = container.querySelector('#small');
-        expect(big && big.textContent).toEqual('Big');
-        expect(small).toBeNull();
+      if (isSmall) {
+        return <p id="small">Small</p>;
+      }
 
-        Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 400 })
+      if (isMedium) {
+        return <p id="medium">Medium</p>;
+      }
 
-        act(() => {
-            window.dispatchEvent(new Event('resize'))
-        })
+      return <p id="big">Big</p>;
+    };
 
-        big = container.querySelector('#big');
-        small = container.querySelector('#small');
-        expect(small && small.textContent).toEqual('Small');
-        expect(big).toBeNull();
-    })
+    act(() => {
+      ReactDOM.render(<Component />, container);
+    });
 
-    it('should switch component based on multiple breakpoints', () => {
-        const Component = () => {
-            const [isSmall, isMedium] = useBreakpoints([500, 700]);
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 400,
+    });
+    act(() => {
+      window.dispatchEvent(new Event('resize'));
+    });
 
-            if (isSmall) {
-                return <p id="small">Small</p>
-            }
+    let big = container.querySelector('#big');
+    let medium = container.querySelector('#medium');
+    let small = container.querySelector('#small');
+    expect(medium).toBeNull();
+    expect(big).toBeNull();
+    expect(small && small.textContent).toEqual('Small');
 
-            if (isMedium) {
-                return <p id="medium">Medium</p>
-            }
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 600,
+    });
+    act(() => {
+      window.dispatchEvent(new Event('resize'));
+    });
 
-            return <p id="big">Big</p>
-        }
-        
-        act(() => {
-            ReactDOM.render(<Component />, container);
-        });
+    big = container.querySelector('#big');
+    medium = container.querySelector('#medium');
+    small = container.querySelector('#small');
+    expect(small).toBeNull();
+    expect(big).toBeNull();
+    expect(medium && medium.textContent).toEqual('Medium');
 
-        Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 400 })
-        act(() => {
-            window.dispatchEvent(new Event('resize'))
-        })
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 800,
+    });
+    act(() => {
+      window.dispatchEvent(new Event('resize'));
+    });
 
-        let big = container.querySelector('#big');
-        let medium = container.querySelector('#medium');
-        let small = container.querySelector('#small');
-        expect(medium).toBeNull();
-        expect(big).toBeNull();
-        expect(small && small.textContent).toEqual('Small');
-
-        Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 600 })
-        act(() => {
-            window.dispatchEvent(new Event('resize'))
-        })
-
-        big = container.querySelector('#big');
-        medium = container.querySelector('#medium');
-        small = container.querySelector('#small');
-        expect(small).toBeNull();
-        expect(big).toBeNull();
-        expect(medium && medium.textContent).toEqual('Medium');
-
-        Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 800 })
-        act(() => {
-            window.dispatchEvent(new Event('resize'))
-        })
-
-        big = container.querySelector('#big');
-        medium = container.querySelector('#medium');
-        small = container.querySelector('#small');
-        expect(small).toBeNull();
-        expect(medium).toBeNull();
-        expect(big && big.textContent).toEqual('Big');
-    })
+    big = container.querySelector('#big');
+    medium = container.querySelector('#medium');
+    small = container.querySelector('#small');
+    expect(small).toBeNull();
+    expect(medium).toBeNull();
+    expect(big && big.textContent).toEqual('Big');
+  });
 });
--- tests/useWindowSize.spec.tsx
+++ tests/useWindowSize.spec.tsx
@@ -5,42 +5,55 @@ import { act } from 'react-dom/test-utils';
 import { useWindowSize } from '../src';
 
 describe('useWindowSize', () => {
-    let container: Element;
+  let container: Element;
 
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+  });
+
+  it('should return the window size', () => {
+    const Component = () => {
+      const { width, height } = useWindowSize();
+
+      return (
+        <>
+          <p id="height">{height}</p>
+          <p id="width">{width}</p>
+        </>
+      );
+    };
+
+    act(() => {
+      ReactDOM.render(<Component />, container);
     });
 
-    afterEach(() => {
-        document.body.removeChild(container);
+    const height = container.querySelector('#height');
+    const width = container.querySelector('#width');
+
+    expect(height && height.textContent).toEqual('768');
+    expect(width && width.textContent).toEqual('1024');
+
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 500,
+    });
+    Object.defineProperty(window, 'innerHeight', {
+      writable: true,
+      configurable: true,
+      value: 200,
     });
 
-    it('should return the window size', () => {
-        const Component = () => {
-            const { width, height } = useWindowSize();
+    act(() => {
+      window.dispatchEvent(new Event('resize'));
+    });
 
-            return <><p id="height">{height}</p><p id="width">{width}</p></>
-        }
-        
-        act(() => {
-            ReactDOM.render(<Component />, container);
-        });
-
-        const height = container.querySelector('#height');
-        const width = container.querySelector('#width');
-
-        expect(height && height.textContent).toEqual('768');
-        expect(width && width.textContent).toEqual('1024');
-
-        Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 500 })
-        Object.defineProperty(window, 'innerHeight', { writable: true, configurable: true, value: 200 })
-
-        act(() => {
-            window.dispatchEvent(new Event('resize'))
-        })
-
-        expect(height && height.textContent).toEqual('200');
-        expect(width && width.textContent).toEqual('500');
-    })
+    expect(height && height.textContent).toEqual('200');
+    expect(width && width.textContent).toEqual('500');
+  });
 });
